ecced4dff8535fd123197c57a10e5f5e0c570ca9
Nada Amin <namin@seas.harvard.edu>
2025-05-03 21:27:11 -0400

    use wandb for tracing

--- agents/README.md
+++ agents/README.md
@@ -5,7 +5,7 @@ The agents rely on the Dafny Sketcher MCP.
 ## Setup
 
 - Make sure the [MCP](../mcp) is built.
-- In your Python environment, `pip install openai-agents`.
+- In your Python environment, `pip install weave openai-agents`.
 - Make sure the environment variable `OPENAI_API_KEY` is set.
 
 ## Run
--- agents/main.py
+++ agents/main.py
@@ -4,6 +4,11 @@ import shutil
 from agents import Agent, Runner, trace
 from agents.mcp import MCPServer, MCPServerStdio
 
+import weave
+from weave.integrations.openai_agents.openai_agents import WeaveTracingProcessor
+from agents import set_trace_processors
+weave.init("openai-agents")
+set_trace_processors([WeaveTracingProcessor()])
 
 async def run(mcp_server: MCPServer):
     agent = Agent(
@@ -12,7 +17,13 @@ async def run(mcp_server: MCPServer):
         mcp_servers=[mcp_server],
     )
 
-    message = "Writes a datatype for arithmetic expressions comprising constants, variables, and binary additions. Then write an evaluator taking an expression and an environment (a function that takes a variable name and returns a number) and returning the number resulting from evaluation. Then write an optimizer taking an expression and returning an expression with all additions by 0 removed. Then prove that the optimizer preserves the semantics as defined by the evaluation function."
+    message = "Write a datatype for arithmetic expressions comprising constants, variables, and binary additions."
+    print("\n" + "-" * 40)
+    print(f"Running: {message}")
+    result = await Runner.run(starting_agent=agent, input=message)
+    print(result.final_output)
+
+    message = "Write a datatype for arithmetic expressions comprising constants, variables, and binary additions. Then write an evaluator taking an expression and an environment (a function that takes a variable name and returns a number) and returning the number resulting from evaluation. Then write an optimizer taking an expression and returning an expression with all additions by 0 removed. Then prove that the optimizer preserves the semantics as defined by the evaluation function."
     print("\n" + "-" * 40)
     print(f"Running: {message}")
     result = await Runner.run(starting_agent=agent, input=message)
