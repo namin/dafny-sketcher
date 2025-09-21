8a1da87955af5fa94e328d19a3664c53c48e65b3
Nada Amin <namin@seas.harvard.edu>
2025-09-21 09:43:22 -0400

    pass in multiple files

--- vfp/bench_driver.py
+++ vfp/bench_driver.py
@@ -1,10 +1,11 @@
 import glob
 import os
 
-def main(main1, print_stats):
-    stats = {} 
-    solution_files = sorted(glob.glob("bench/*_solution.dfy"))
-    solution_files = [f for f in solution_files if os.path.basename(f)[0].islower()]
+def main(main1, print_stats, solution_files=None):
+    stats = {}
+    if not solution_files:
+        solution_files = sorted(glob.glob("bench/*_solution.dfy"))
+        solution_files = [f for f in solution_files if os.path.basename(f)[0].islower()]
     print(len(solution_files))
     print(solution_files)
     for f in solution_files:
@@ -16,16 +17,9 @@ def run(main1, print_stats):
     import argparse
     
     parser = argparse.ArgumentParser(description='Run the bench suite')
-    parser.add_argument('--file', type=str, help='Path to Dafny file to process')
+    parser.add_argument('--file', type=str, action='append', help='Path to Dafny file to process (can be used multiple times)')
     
     args = parser.parse_args()
     
-    if args.file:
-        stats = {}
-        # run on a single file
-        f = args.file
-        main1(f, stats)
-        print_stats(stats)
-    else:
-        main(main1, print_stats)
-        
+    main(main1, print_stats, args.file)
+
