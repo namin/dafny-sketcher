51c91ff0d6b632e682c7ab75227f0218a0e39260
Nada Amin <namin@seas.harvard.edu>
2025-09-24 10:35:36 -0400

    add lemma switch

--- vfp/bench_driver.py
+++ vfp/bench_driver.py
@@ -3,7 +3,7 @@ import os
 import tests
 import sketcher
 
-def main(lemma1, print_stats, solution_files=None):
+def main(lemma1, print_stats, solution_files=None, lemma_names=None):
     stats = {}
     if not solution_files:
         solution_files = sorted(glob.glob("bench/*_solution.dfy"))
@@ -11,10 +11,10 @@ def main(lemma1, print_stats, solution_files=None):
     print(len(solution_files))
     print(solution_files)
     for f in solution_files:
-        main1(lemma1, f, stats)
+        main1(lemma1, f, stats, lemma_names=lemma_names)
     print_stats(stats)
 
-def main1(lemma1, f, stats):
+def main1(lemma1, f, stats, lemma_names=None):
     print('---------- Looking at file: ', f, '--------------')
     p = tests.read_file(f)
     if False:
@@ -25,15 +25,17 @@ def main1(lemma1, f, stats):
     done = sketcher.sketch_done(p)
     lemmas = [x for x in done if x['type'] == 'lemma']
     for lemma in lemmas:
-        lemma1(lemma, p, stats)
+        if not lemma_names or lemma['name'] in lemma_names:
+            lemma1(lemma, p, stats)
 
 def run(lemma1, print_stats):
     import argparse
     
     parser = argparse.ArgumentParser(description='Run the bench suite')
     parser.add_argument('--file', type=str, action='append', help='Path to Dafny file to process (can be used multiple times)')
+    parser.add_argument('--lemma', type=str, action='append', help='Name of lemma to process (can be used multiple times)')
     
     args = parser.parse_args()
     
-    main(lemma1, print_stats, args.file)
+    main(lemma1, print_stats, args.file, args.lemma)
 
