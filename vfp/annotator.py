a0e053c90045a87866561fb3423e8b4e303b5d83
Nada Amin <namin@seas.harvard.edu>
2025-09-28 13:00:55 -0400

    more tweaks

--- vfp/annotator.py
+++ vfp/annotator.py
@@ -1,10 +1,11 @@
 import requests
 import os
+import argparse
 
 SERVER = os.environ['DAFNY_ANNOTATOR_SERVER']
 
-def greedy_search(program: str):
-    url = f"{SERVER}/greedy_search"
+def endpoint(path: str, program: str):
+    url = f"{SERVER}/{path}"
     params = {"program": program}
     response = requests.post(url, params=params)
     
@@ -13,14 +14,28 @@ def greedy_search(program: str):
     
     return response.json()
 
-if __name__ == "__main__":
-    import tests
-    import sys
-    if len(sys.argv) > 1:
-        f = sys.argv[1]
-        p = tests.read_file(f)
+def greedy_search(program: str):
+    return endpoint("greedy_search", program)
+
+def annotate(program: str):
+    return endpoint("annotate", program)
+
+def main():
+    parser = argparse.ArgumentParser(description='Run greedy search on Dafny programs')
+    parser.add_argument('file', nargs='?', help='Dafny file to process')
+    parser.add_argument('--program', '-p', help='Dafny program as string (alternative to file)')
+    parser.add_argument('--endpoint', '-e', choices=['greedy_search', 'annotate'], help='Endpoint to use', default='greedy_search')
+    
+    args = parser.parse_args()
+    
+    if args.file:
+        import tests
+        program = tests.read_file(args.file)
+    elif args.program:
+        program = args.program
     else:
-        p = """
+        # Default program if no file or program specified
+        program = """
     method SquareRoot(n: nat)
     returns (r: nat)
     ensures r*r <= n <= (r+1)*(r+1)
@@ -32,4 +47,9 @@ if __name__ == "__main__":
         }
     }
     """
-    print(greedy_search(p))
+    
+    result = endpoint(args.endpoint, program)
+    print(result)
+
+if __name__ == "__main__":
+    main()
